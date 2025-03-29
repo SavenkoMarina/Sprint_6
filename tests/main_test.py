edda8e630d0f8289aed7c4e85550f2ffcbc0db2b
savenko <mnedozrelova@gmail.com>
2025-03-29 19:04:19 +0100

    fix after review

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,3 +1,4 @@
+import pytest
 from allure import title
 
 from data import urls
@@ -6,69 +7,24 @@ from locators.main_page_locators import MainPageLocators
 
 
 class TestQuestions:
-    @title("Проверка открытия первого вопроса")
-    def test_first_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.FIRST_QUESTION,
-            MainPageLocators.FIRST_ANSWER
-        )
 
-    @title("Проверка открытия второго вопроса")
-    def test_second_question(self, driver):
+    @pytest.mark.parametrize(
+        "question, answer",
+        [
+            (MainPageLocators.FIRST_QUESTION, MainPageLocators.FIRST_ANSWER),
+            (MainPageLocators.SECOND_QUESTION, MainPageLocators.SECOND_ANSWER),
+            (MainPageLocators.THIRD_QUESTION, MainPageLocators.THIRD_ANSWER),
+            (MainPageLocators.FOURTH_QUESTION, MainPageLocators.FOURTH_ANSWER),
+            (MainPageLocators.FIFTH_QUESTION, MainPageLocators.FIFTH_ANSWER),
+            (MainPageLocators.SIXTH_QUESTION, MainPageLocators.SIXTH_ANSWER),
+            (MainPageLocators.SEVENTH_QUESTION, MainPageLocators.SEVENTH_ANSWER),
+            (MainPageLocators.EIGHTH_QUESTION, MainPageLocators.EIGHTH_ANSWER),
+        ]
+    )
+    @title("Проверка открытия вопроса")
+    def test_first_question(self, driver, question, answer):
         page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.SECOND_QUESTION,
-            MainPageLocators.SECOND_ANSWER
-        )
-
-    @title("Проверка открытия третьего вопроса")
-    def test_third_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.THIRD_QUESTION,
-            MainPageLocators.THIRD_ANSWER
-        )
-
-    @title("Проверка открытия четвертого вопроса")
-    def test_fourth_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.FOURTH_QUESTION,
-            MainPageLocators.FOURTH_ANSWER
-        )
-
-    @title("Проверка открытия пятого вопроса")
-    def test_fifth_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.FIFTH_QUESTION,
-            MainPageLocators.FIFTH_ANSWER
-        )
-
-    @title("Проверка открытия шестого вопроса")
-    def test_sixth_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.SIXTH_QUESTION,
-            MainPageLocators.SIXTH_ANSWER
-        )
-
-    @title("Проверка открытия седьмого вопроса")
-    def test_seventh_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.SEVENTH_QUESTION,
-            MainPageLocators.SEVENTH_ANSWER
-        )
-
-    @title("Проверка открытия восьмого вопроса")
-    def test_eighth_question(self, driver):
-        page = MainPage(driver)
-        page.open_question_and_check_answer(
-            MainPageLocators.EIGHTH_QUESTION,
-            MainPageLocators.EIGHTH_ANSWER
-        )
+        page.open_question_and_check_answer(question, answer)
 
     @title('Клик на логотип "Самокат"')
     def test_click_logo_scooter(self, driver):
